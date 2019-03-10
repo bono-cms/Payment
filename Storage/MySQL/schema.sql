8e28af65c549b5889a28371c356c5bf4dabb1a23
dave-yang <daworld.ny@gmail.com>
2019-03-10 23:47:23 +0500

    Added initial mapper

---
+++ Storage/MySQL/schema.sql
@@ -0,0 +1,10 @@
+
+DROP TABLE IF EXISTS `bono_module_payment_transactions`;
+
+CREATE TABLE bono_module_payment_transactions (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `amount` FLOAT NOT NULL COMMENT 'Amount',
+    `datetime` DATETIME NOT NULL COMMENT 'Date and time',
+    `status` SMALLINT COMMENT 'Status constant',
+    `module` varchar(255) NOT NULL COMMENT 'Module where payment was made'
+);
