0ec1d63cfd5cfa1ac51dd12bdd30df8731a9fe73
dave-yang <daworld.ny@gmail.com>
2019-03-26 22:00:59 +0500

    Added save, edit and add actions with their routes

--- Config/routes.php
+++ Config/routes.php
@@ -5,6 +5,18 @@ return array(
         'controller' => 'Admin:Transaction@indexAction'
     ),
 
+    '/%s/module/payment/save' => array(
+        'controller' => 'Admin:Transaction@saveAction'
+    ),
+
+    '/%s/module/payment/add' => array(
+        'controller' => 'Admin:Transaction@addAction'
+    ),
+
+    '/%s/module/payment/edit/(:var)' => array(
+        'controller' => 'Admin:Transaction@editAction'
+    ),
+
     '/%s/module/payment/delete/(:var)' => array(
         'controller' => 'Admin:Transaction@deleteAction'
     ),
--- Controller/Admin/Transaction.php
+++ Controller/Admin/Transaction.php
@@ -12,9 +12,75 @@
 namespace Payment\Controller\Admin;
 
 use Cms\Controller\Admin\AbstractController;
+use Krystal\Stdlib\VirtualEntity;
 
 final class Transaction extends AbstractController
 {
+    /**
+     * Creates a form
+     * 
+     * @param \Krystal\Stdlib\VirtualEntity $transaction
+     * @return string
+     */
+    private function createForm(VirtualEntity $transaction)
+    {
+        // Append a breadcrumb
+        $this->view->getBreadcrumbBag()
+                   ->addOne('Payments', 'Payment:Admin:Transaction@indexAction')
+                   ->addOne($transaction->getId() ? 'Edit the transaction' : 'Add new transaction');
+
+        return $this->view->render('form', array(
+            'transaction' => $transaction
+        ));
+    }
+
+    /**
+     * Saves transaction
+     * 
+     * @return string
+     */
+    public function saveAction()
+    {
+        // Get raw POST data
+        $input = $this->request->getPost('transaction');
+
+        $transactionService = $this->getModuleService('transactionService');
+        $transactionService->save($input);
+
+        if ($input['id']) {
+            return 1;
+        } else {
+            return $transactionService->getLastId();
+        }
+    }
+
+    /**
+     * Renders add form
+     * 
+     * @return string
+     */
+    public function addAction()
+    {
+        return $this->createForm(new VirtualEntity);
+    }
+
+    /**
+     * Renders edit form
+     * 
+     * @param int $id Transaction id
+     * @return string
+     */
+    public function editAction($id)
+    {
+        $transaction = $this->getModuleService('transactionService')->fetchById($id);
+
+        if ($transaction) {
+            return $this->createForm($transaction);
+        } else {
+            return false;
+        }
+    }
+
     /**
      * Renders main grid
      * 
