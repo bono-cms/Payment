a56f96bb4ee13fb9a8a9a9ae443a7c20e1145c72
dave-yang <daworld.ny@gmail.com>
2019-03-29 00:11:44 +0500

    Added Prime4G extension
    Initial implementation

---
+++ Extension/Prime4G/Prime4GFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Payment\Extension\Prime4G;
+
+final class Prime4GFactory
+{
+    /**
+     * Builds Prime4GFactory web form generator
+     * 
+     * @param mixed $price The price
+     * @param int $orderId Order ID
+     * @param string $back Back URL on success
+     * @param array $credits
+     * @return self
+     */
+    public function build($price, int $orderId, string $back, array $options)
+    {
+        $options = array_merge(include(__DIR__ . '/config.php'), $options);
+
+        return new WebForm([
+            'OrderID' => $orderId,
+            'Password' => $options['Password'],
+            'MerID' => $options['MerID'],
+            'AcqID' => $options['AcqID'],
+            'PurchaseAmt' => WebForm::normalizePrice($price),
+            'MerRespURL' => $back,
+            'action' => $options['action']
+        ]);
+    }
+}
